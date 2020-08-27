620f8d0023c7d58b6b2e8768098dc8dc127a67e6
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-27 17:00:13 -0400

    Creacion de components pregunta y formato del sitio

---
+++ src/components/Pregunta.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+const Pregunta = () => {
+    return (
+        <Fragment>
+            <h2>Coloca tu Presupuesto</h2>
+            <form>
+                <input
+
+                    type='number'
+                    className='u-full-width'
+                    placeholder='coloca tus gastos'
+                />
+                <input
+
+                    type='submit'
+                    className='button-primary u-full-width'
+                    value='definir presupuesto'
+                />
+            </form>
+        </Fragment>
+    );
+}
+
+export default Pregunta;
