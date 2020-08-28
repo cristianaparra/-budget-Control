8b60f366ef3b86c0248b54cd649e8901c8b573d8
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-27 22:17:52 -0400

    agregando modulo short id

---
+++ q
@@ -0,0 +1,12 @@
+creando state de gastos
+
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# Date:      Thu Aug 27 20:46:30 2020 -0400
+#
+# On branch master
+# Changes to be committed:
+#	modified:   src/components/Formulario.js
+#
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,31 +1,43 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import Error from './Error'
+import shortid from 'shortid'
 
 const Formulario = () => {
 
-const [nombre, guardarNombre] = useState('');
-const [cantidad, guardarCantidad] = useState(0);
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
 
 
-const agregarGasto= e =>{
-    e.preventDefault();
+    const agregarGasto = e => {
+        e.preventDefault();
 
-    //validar
+        //validar
+        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === "") {
+            guardarError(true);
+            return
+        }
 
-
-    //contruir el gasto
-
-    //pasar el gastyo a nuestro componente
-
-    //resetear form
-
+        //contruir el gasto
+const gasto ={
+    nombre,
+    cantidad,
+    id:shortid.generate()
 }
+console.log(gasto)
+        //pasar el gastyo a nuestro componente
+
+        //resetear form
+
+    }
 
     return (
         <form
-        onSubmit={agregarGasto}
+            onSubmit={agregarGasto}
         >
             <h2>Agrega tus gastos</h2>
 
+            { error ? <Error mensaje='Ambos campos son obligatoriios o Presupuesto incorrecto' /> : null } 
             <div className='campo'>
                 <label>Nombre gastos</label>
                 <input
@@ -48,10 +60,10 @@ const agregarGasto= e =>{
                 />
             </div>
 
-            <input 
-            type='submit'
-            className='button-primary u-full-width'
-            value='Agregar Gastos'
+            <input
+                type='submit'
+                className='button-primary u-full-width'
+                value='Agregar Gastos'
             />
         </form>
 
