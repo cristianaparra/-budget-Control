0f21058f62f606e8076e9bf0f3cec01b4ad87f71
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-27 20:46:30 -0400

    validando formularios

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Formulario = () => {
+
+const [nombre, guardarNombre] = useState('');
+const [cantidad, guardarCantidad] = useState(0);
+
+
+const agregarGasto= e =>{
+    e.preventDefault();
+
+    //validar
+
+
+    //contruir el gasto
+
+    //pasar el gastyo a nuestro componente
+
+    //resetear form
+
+}
+
     return (
-        <form>
+        <form
+        onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos</h2>
 
             <div className='campo'>
@@ -11,6 +32,8 @@ const Formulario = () => {
                     type='text'
                     className='u-full-width'
                     placeholder='Ejemplo, Comida'
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
 
@@ -20,6 +43,8 @@ const Formulario = () => {
                     type='number'
                     className='u-full-width'
                     placeholder='Ejemplo, 50.000'
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value), 10)}
                 />
             </div>
 
